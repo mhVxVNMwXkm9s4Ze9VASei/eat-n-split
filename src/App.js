4e65c84ca7d7aeccdcac3d64927eecb5a6745d1e
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-08-03 17:54:03 -0500

    Turned form elements into controlled elements

--- src/App.js
+++ src/App.js
@@ -167,24 +167,48 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+	const [bill, setBill] = useState("");
+	const [paidByUser, setPaidByUser] = useState("");
+	const [payer, setPayer] = useState("user");
+
+	const paidByFriend = bill ? bill - paidByUser + selectedFriend.balance : "";
+
 	return (
 		<form className="form-split-bill">
 			<h2>Split a bill with {selectedFriend.name}.</h2>
 
 			<label>💱 Bill value</label>
-			<input type="text" />
+			<input
+				type="text"
+				value={bill}
+				onChange={(event) => setBill(Number(event.target.value))}
+			/>
 
 			<label>🧍‍♂️ Your expense</label>
-			<input type="text" />
+			<input
+				type="text"
+				value={paidByUser}
+				onChange={(event) =>
+					setPaidByUser(
+						Number(event.target.value) > bill
+							? paidByUser
+							: Number(event.target.value)
+					)
+				}
+			/>
 
 			<label>👫 {selectedFriend.name}'s expense</label>
 			<input
 				type="text"
+				value={paidByFriend}
 				disabled
 			/>
 
 			<label>🤑 Who is paying the bill?</label>
-			<select>
+			<select
+				value={payer}
+				onChange={(event) => setPayer(event.target.value)}
+			>
 				<option value="user">You</option>
 				<option value="friend">{selectedFriend.name}</option>
 			</select>
